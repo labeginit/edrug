cbc2bd7afd590002309b9f67a5beac6805beca3e
Samuel Mcmurray <samuel_joseph.mcmurray0004@stud.hkr.se>
2020-04-16 14:35:38 +0200

    start of registration page

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -3,9 +3,14 @@ package controller;
 import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.net.URL;
@@ -42,14 +47,47 @@ public class LoginController implements Initializable {
     @FXML public void loginButtonPressed(ActionEvent ae) {
         progress.setVisible(true);
         PauseTransition pt = new PauseTransition();
-        pt.setDuration(Duration.seconds(3));
+        pt.setDuration(Duration.seconds(2));
         pt.setOnFinished(event -> {
             System.out.println("Login successful");
+            try {
+                Node node = (Node) ae.getSource();
+                Scene scene = node.getScene();
+                Stage stage = (Stage) scene.getWindow();
 
+                Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
+                Scene newScene = new Scene(root);
+
+                stage.setTitle("e-Drugs");
+                stage.setScene(newScene);
+
+
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         });
         pt.play();
     }
     @FXML public void registerButtonPressed(ActionEvent ae) {
+        progress.setVisible(true);
+        PauseTransition pt = new PauseTransition();
+        pt.setDuration(Duration.seconds(2));
+        pt.setOnFinished(event -> {
+        try {
+            Node node = (Node) ae.getSource();
+            Scene scene = node.getScene();
+            Stage stage = (Stage) scene.getWindow();
 
+            Parent root = FXMLLoader.load(getClass().getResource("/view/registration.fxml"));
+            Scene newScene = new Scene(root);
+
+            stage.setTitle("e-Drugs Registration");
+            stage.setScene(newScene);
+
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+        });
+        pt.play();
     }
 }
