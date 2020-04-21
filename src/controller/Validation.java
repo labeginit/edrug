bed1ef6bdd9482507ee90d625071c77379b1a054
liliia <liliia.allansson0085@stud.hkr.se>
2020-04-21 09:36:48 +0200

    example validation method added

--- src/controller/Validation.java
+++ src/controller/Validation.java
@@ -4,11 +4,20 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 
 public class Validation {
-    static void alertPopup(String infoMessage, String titleBar, String headerMessage) {
+    public static void alertPopup(String infoMessage, String titleBar, String headerMessage) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION, infoMessage, ButtonType.OK);
         alert.setResizable(true);
         alert.setTitle(titleBar);
         alert.setHeaderText(headerMessage);
         alert.showAndWait();
+    }
+
+    //an example of validation method
+    public static boolean validateAmount(String value){
+        String reg = "^(?!0+$)\\d+$";
+        if (!value.matches(reg)){
+            alertPopup("Wrong input format", "Illegal format", "Wrong input format");
+        }
+        return value.matches(reg);
     }
 }
