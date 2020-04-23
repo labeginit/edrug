3d50535e1eb67e16abcd21f6dbfe1be4863ff947
liliia <liliia.allansson0085@stud.hkr.se>
2020-04-23 14:30:13 +0200

    removed an unused variable

--- src/controller/RegistrationController.java
+++ src/controller/RegistrationController.java
@@ -27,7 +27,7 @@ import java.sql.Date;
 
 public class RegistrationController implements Initializable {
     CommonMethods common = new CommonMethods();
-    private User user;
+    
     @FXML
     private Button registerButton;
 
