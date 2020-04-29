b99e8a0af83ea9e67857cd563f6d8668efa06399
Fanny <fannysarasoderlund@gmail.com>
2020-04-29 21:51:08 +0200

    filled profile admin

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -8,11 +8,14 @@ import javafx.scene.control.*;
 
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import model.Admin;
 import model.CommonMethods;
 
 import model.User;
 import model.UserSingleton;
+
 import java.net.URL;
+import java.sql.Date;
 import java.util.ResourceBundle;
 
 public class AdminController implements Initializable {
@@ -20,11 +23,6 @@ public class AdminController implements Initializable {
 
     public ComboBox filterPatient;
     public ComboBox doctorFilter;
-    public TreeTableColumn addSSN;
-    public TreeTableColumn addName;
-    public TreeTableColumn addPhone;
-    public TreeTableColumn addEmail;
-    public TreeTableColumn addRole;
     public ComboBox addFilter;
     public Button confirmAddButton;
     public TextField SSNnewTextField;
@@ -63,15 +61,65 @@ public class AdminController implements Initializable {
     public TableColumn<User, String> changeSSNTable;
     public TableView<User> changeTable;
     public TableColumn<User, Integer> changeRoleTable;
+    public Label SSNstar;
+    public Label BDateStar;
+    public Label firstNameStar;
+    public Label lastNameStar;
+    public Label zipStar;
+    public Label addressStar;
+    public Label phoneStar;
+    public Label emailStar;
+    public Label pass2star;
+    public Label pass1star;
 
     CommonMethods methods = new CommonMethods();
     User currentUser = UserSingleton.getOurInstance().getUser();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        SSNstar.setVisible(false);
+        BDateStar.setVisible(false);
+        firstNameStar.setVisible(false);
+        lastNameStar.setVisible(false);
+        zipStar.setVisible(false);
+        addressStar.setVisible(false);
+        phoneStar.setVisible(false);
+        emailStar.setVisible(false);
+        pass1star.setVisible(false);
+        pass2star.setVisible(false);
+
         fillPatientTable();
         fillDoctorTable();
         fillEditTable();
+        fillMe();
+
+        cancel_button.setOnAction(actionEvent -> {
+            fillMe();
+        });
+        save_button.setOnAction(actionEvent -> {
+            if (isItOk()) {
+            if (Validation.isName(firstName_text.getText(), firstNameStar) && Validation.isName(lastName_text.getText(), lastNameStar) &&
+                    Validation.isZipcode(zip_text.getText(), zipStar) && Validation.isPhoneNumber(phone_text.getText(), phoneStar)
+                    && Validation.isEmail(email_text.getText(), emailStar)) {
+                try {
+                    currentUser.setSsn(SSNtext.getText());
+                    currentUser.setLastName(lastName_text.getText());
+                    currentUser.setBDate(Date.valueOf(birth_text.getText()));
+                    currentUser.setZipCode(zip_text.getText());
+                    currentUser.setAddress(address_text.getText());
+                    currentUser.setPhoneNumber(phone_text.getText());
+                    currentUser.setEmail(email_text.getText());
+                    currentUser.setPassword(pass1_text.getText());
+                    methods.updateAdmin((Admin) currentUser);
+
+                    pass1_text.setText("******");
+                    pass2_text.setText("******");
+
+                } catch (Exception e ) {
+                    e.printStackTrace();
+                }
+            }
+            }});
 
     }
 
@@ -116,7 +164,67 @@ public class AdminController implements Initializable {
         ObservableList<User> listOfAll = FXCollections.observableArrayList(methods.getDoctorList());
         listOfAll.addAll(FXCollections.observableArrayList(methods.getPatientList()));
 
-            changeTable.setItems(listOfAll);
+        changeTable.setItems(listOfAll);
 
+    }
+
+    public void fillMe() {
+        SSNtext.setText(currentUser.getSsn());
+        birth_text.setText(currentUser.getBDate().toString());
+        firstName_text.setText(currentUser.getFirstName());
+        lastName_text.setText(currentUser.getLastName());
+        zip_text.setText(currentUser.getZipCode());
+        address_text.setText(currentUser.getAddress());
+        phone_text.setText(currentUser.getPhoneNumber());
+        email_text.setText(currentUser.getEmail());
+        pass1_text.setText("******");
+        pass2_text.setText("******");
+
+    }
+
+    private boolean isItOk() {
+        if (firstName_text.getText().isEmpty() || lastName_text.getText().isEmpty() || birth_text.getText().isEmpty()
+                || zip_text.getText().isEmpty() || address_text.getText().isEmpty() || email_text.getText().isEmpty() || phone_text.getText().isEmpty()) {
+            if (firstName_text.getText().isEmpty()) {
+                firstNameStar.setVisible(true);
+            }
+            if (lastName_text.getText().isEmpty()) {
+                lastNameStar.setVisible(true);
+            }
+            if (birth_text.getText().isEmpty()) {
+                BDateStar.setVisible(true);
+            }
+            if (zip_text.getText().isEmpty()) {
+                zipStar.setVisible(true);
+            }
+            if (address_text.getText().isEmpty()) {
+                addressStar.setVisible(true);
+            }
+            if (email_text.getText().isEmpty()) {
+                emailStar.setVisible(true);
+            }
+            if (phone_text.getText().isEmpty()) {
+                phoneStar.setVisible(true);
+            }
+            if (pass1_text.getText().isEmpty()) {
+                pass1star.setVisible(true);
+            }
+            if (pass2_text.getText().isEmpty()) {
+                pass2star.setVisible(true);
+            }
+            Validation.alertPopup("Please enter your information into all fields", "Empty Fields", "Contains empty fields");
+        return false;
+        } else if (!pass1_text.getText().equals(pass2_text.getText())) {
+            pass1star.setVisible(true);
+            pass2star.setVisible(true);
+            Validation.alertPopup("Password does not match", "Password Mismatch", "Password doesn't match");
+return false;
+        }
+        else if (!Validation.isPassword(pass1_text.getText(), pass1star)) {
+        return false;
+        }
+        else {
+            return true;
         }
     }
+}
