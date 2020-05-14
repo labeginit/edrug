7842e1dcca43f31b7c2d58fed40784d8b646b09a
liliia <liliia.allansson0085@stud.hkr.se>
2020-05-14 11:58:05 +0200

    Removed commented code

--- src/model/dBConnection/DAOPrescription.java
+++ src/model/dBConnection/DAOPrescription.java
@@ -67,58 +67,6 @@ public class DAOPrescription {
         }
     }
 
-/*  WIP
-    protected List<Prescription> getPrescriptionList(Doctor doctor) {
-        List<Prescription> prescList = null;
-        String query = "select id, patient_ssn, user_ssn, date, diagnosis, article, quantity, instructions from Prescription \n" +
-                "join Prescription_has_Medicine on id = prescription_id and prescription_patient_ssn = patient_ssn\n" +
-                "where user_ssn = ? order by id, prescription_patient_ssn;";
-        prescription = null;
-        specification.clear();
-        try {
-            if (!DBConnection.dbConnection.isClosed()) {
-                resultSet = common.retrieveSet(query, doctor.getSsn());
-                if (resultSet != null) {
-                    while (resultSet.next()) {
-                        id = resultSet.getInt("id");
-                      //  if (id == )
-                        this.doctorSSN = resultSet.getString("user_ssn");
-                        patientSSN = resultSet.getString("patient_ssn");
-                        date = resultSet.getDate("date");
-                        diagnosis = resultSet.getString("diagnosis");
-                        prescription = new Prescription(id, (Doctor) user.getUser(doctorSSN), (Patient) user.getUser(patientSSN), date, diagnosis, specification);
-                        prescList.add(prescription);
-
-                        int prescId = resultSet.getInt("prescription_id");
-                        String prescPatientSSN = resultSet.getString("prescription_patient_ssn");
-                        article = resultSet.getInt("article");
-                        quantity = resultSet.getInt("quantity");
-                        instructions = resultSet.getString("instructions");
-
-                        line = new PrescriptionLine(id, (Patient) user.getUser(prescPatientSSN), (Medicine) med.getMedicine(article), quantity, instructions);
-                        specification.add(line);
-
-                    }
-                } else {
-                    System.out.println("Empty resultSet");
-                    return prescList;
-                }
-            }
-        } catch (SQLException ex) {
-            System.out.println("Error while working with ResultSet!");
-            ex.printStackTrace();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            try {
-                resultSet.close();
-            }catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return user;
-        }
-    }
-*/
 protected List<PrescriptionLine> retrievePrescriptionLines(int prescriptionId, User currentUser) {
     specification.clear();
     try {
